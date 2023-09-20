a7afdcaeaf5fb992a2834c4a0831b96f9a8ab01e
Fuguo Xue <fx2100@nyu.edu>
2023-09-20 15:05:38 -0400

    add a square that can change color based on mouseX

--- sketch.js
+++ sketch.js
@@ -1,5 +1,30 @@
+let xpos = 100;
+let width = 600;
+let height = 600;
+
 function setup() {
+    createCanvas(width, height);
 }
 
 function draw() {
+    background(125,125,200);
+    strokeWeight(0);
+
+    // rect(xpos, 10,30,30);
+    // rect(xpos, 50,30,30);
+    // rect(xpos, 90,30,30);
+
+    // rect(mouseX, mouseY, 30, 30);
+
+    // print in p5.js
+    // print(mouseX, mouseY);
+    // print in JS
+    // console.log(mouseX, mouseY);
+
+    if (mouseX < width/2){
+        fill(255, 125,125);
+    } else {
+        fill(125,255,125);
+    }
+    rect(mouseX, mouseY, 30, 30);
 }
